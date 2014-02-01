adff1eab37a3339b0c281a6288ee073ba302305b
Elmer Thomas <elmer@ThinkingSerious.com>
2014-01-31 18:07:44 -0800

    Fixed some formatting issues

--- src/com/thinkingserious/sendgrid/glass/gdk/example/SendEmailWithSendGrid.java
+++ src/com/thinkingserious/sendgrid/glass/gdk/example/SendEmailWithSendGrid.java
@@ -10,10 +10,11 @@ import com.github.sendgrid.SendGrid;
 public class SendEmailWithSendGrid extends AsyncTask<Hashtable<String,String>, Void, String> {
     Utils creds = new Utils();
 
-	@Override
+    @Override
     protected String doInBackground(Hashtable<String,String>... params) {
             Hashtable<String,String> h = params[0];
             SendGrid sendgrid = new SendGrid(creds.getUsername(),creds.getPassword());
+            
             sendgrid.setFrom(creds.getFromAddress());
             sendgrid.addTo(h.get("to"));
             sendgrid.setSubject(h.get("subject"));
